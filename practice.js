81cb24c7ef46f8f06cae4aecc09e67e847baac3f
Sreeram Govindan Rajendran <sreerambeece@gmail.com>
2024-12-08 00:29:13 +0530

    practice js

---
+++ practice.js
@@ -0,0 +1,22 @@
+console.log("Practicing JavaScript")
+
+var a = "My name is Sreeram"
+console.log(a)
+
+var a = "Ravi"
+console.log(a)
+
+let b = 2
+console.log(b)
+
+const istrue = true
+console.log(istrue)
+console.log(!istrue)
+
+//istrue = false
+
+for(k=0;k<10;k++){
+    if(istrue){
+        console.log(k)
+    }
+}
