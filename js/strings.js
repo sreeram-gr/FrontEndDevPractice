1bd2ff7b863d7b28682be68774ce108f46ea117e
Sreeram GR <sreerambeece@gmail.com>
2025-06-19 14:30:34 +0530

    Updated with Freecodecamp and TheOdinProject assignments

--- js/strings.js
+++
@@ -1,8 +0,0 @@
-let username = prompt("Enter the username");
-
-if(username!==null && username!==""){
-    alert(`your new username is @${username+username.length}`);
-}else{
-    username = prompt("Enter the valid username");
-    alert(`your new username is @${username+username.length}`);
-}
